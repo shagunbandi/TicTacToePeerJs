30a360a876c4673b3107a552a03df5d1df841084
Shagun Bandi <shagunamitbandi@gmail.com>
2020-11-17 15:42:35 +0530

    Init

--- src/components/Game/index.jsx
+++ src/components/Game/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import Peer from "peerjs";
 
 import { initialSetup } from "../../store/actions/appActions";
 
@@ -11,8 +12,10 @@ export class Game extends Component {
     this.state = {
       size: 3,
       streak: 3,
+      peerId: "",
       err: "",
       setupDone: false,
+      waiting: false,
     };
   }
 
@@ -30,18 +33,77 @@ export class Game extends Component {
     });
   };
 
-  onSubmitHandler = (e) => {
+  onPeerIdChangeHandler = (e) => {
+    this.setState({
+      peerId: e.target.value,
+    });
+  };
+
+  sendData = (conn) => {
+    if (conn == null) {
+      console.log("Null");
+    }
+    conn.send("Hi");
+  };
+
+  initialize = () => {
+    let peer = new Peer({
+      host: "127.0.0.1",
+      port: "9000",
+      path: "/myapp",
+    });
+
+    peer.on("open", (id) => {
+      this.setState({ newPeerId: id });
+    });
+    return peer;
+  };
+
+  onNewGameSubmitHandler = (e) => {
     e.preventDefault();
     const { size, streak } = this.state;
-    if (size === "" || streak === "") {
+    if (
+      size === "" ||
+      streak === "" ||
+      parseInt(streak) <= 0 ||
+      parseInt(size) <= 0
+    ) {
       this.setState({ err: "Size and Streak cannot be empty" });
     } else if (parseInt(streak) > parseInt(size)) {
       this.setState({ err: "Streak cannot be greater than size" });
     } else {
-      console.log("Success with values", size, streak);
       this.props.initialSetup(size, streak, this.getGrid(size));
-      this.setState({ setupDone: true });
+
+      this.setState({ setupDone: true, waiting: true });
+      let peer = this.initialize();
+
+      peer.on("connection", (c) => {
+        console.log("Connected to: " + c.peer);
+        c.on("data", (data) => {
+          console.log("Data recieved", data);
+        });
+        // this.setState({ waiting: false });
+      });
     }
+  };
+
+  onJoinGameSubmitHandler = (e) => {
+    e.preventDefault();
+    let conn = null;
+
+    let peer = this.initialize();
+    conn = peer.connect(this.state.peerId, {
+      host: "127.0.0.1",
+      port: "9000",
+      path: "/myapp",
+      reliable: true,
+    });
+
+    conn.on("open", () => {
+      conn.on("data", (data) => {
+        console.log("Data recieved", data);
+      });
+    });
   };
 
   getGrid = (size) => {
@@ -56,39 +118,78 @@ export class Game extends Component {
   };
 
   render() {
+    if (this.state.waiting) {
+      return (
+        <div>
+          <h3>Waiting for opponenent ...</h3>
+          <br />
+          <h4>Peer Id is {this.state.newPeerId}</h4>
+        </div>
+      );
+    }
     if (this.state.setupDone) {
       return <Play />;
     }
     return (
-      <div className='row align-items-center'>
-        <form onSubmit={(e) => this.onSubmitHandler(e)}>
-          <div class='form-group'>
-            <label for='exampleInputEmail1'>Size of the grid</label>
-            <input
-              type='number'
-              class='form-control'
-              id='size'
-              value={this.state.size}
-              onChange={this.onSizeChangeHandler}
-              placeholder='Enter Size'
-            />
-          </div>
-          <div class='form-group'>
-            <label for='exampleInputPassword1'>Streak length</label>
-            <input
-              type='number'
-              class='form-control'
-              id='streak'
-              value={this.state.streak}
-              onChange={this.onStreakChangeHandler}
-              placeholder='Length of the Streak'
-            />
-          </div>
-          <button type='submit' class='btn btn-primary'>
-            Submit
-          </button>
-          <small class='form-text text-danger'>{this.state.err}</small>
-        </form>
+      <div className='container'>
+        <div className='row'>
+          <h3>Host a new Game</h3>
+        </div>
+        <br />
+        <div className='row'>
+          <form onSubmit={(e) => this.onNewGameSubmitHandler(e)}>
+            <div className='form-group'>
+              <label>Size of the grid</label>
+              <input
+                type='number'
+                className='form-control'
+                id='size'
+                value={this.state.size}
+                onChange={this.onSizeChangeHandler}
+                placeholder='Enter Size'
+              />
+            </div>
+            <div className='form-group'>
+              <label>Streak length</label>
+              <input
+                type='number'
+                className='form-control'
+                id='streak'
+                value={this.state.streak}
+                onChange={this.onStreakChangeHandler}
+                placeholder='Length of the Streak'
+              />
+            </div>
+            <button type='submit' className='btn btn-primary'>
+              Start a New Game
+            </button>
+            <small className='form-text text-danger'>{this.state.err}</small>
+          </form>
+        </div>
+        <hr />
+        <div className='row'>
+          <h3>Join a Game</h3>
+        </div>
+        <br />
+        <div className='row'>
+          <form onSubmit={this.onJoinGameSubmitHandler}>
+            <div className='form-group'>
+              <label>Peer Id</label>
+              <input
+                type='text'
+                className='form-control'
+                id='peerId'
+                value={this.state.peerId}
+                onChange={this.onPeerIdChangeHandler}
+                placeholder='Enter Peer Id'
+              />
+            </div>
+            <button type='submit' className='btn btn-primary'>
+              Play a friend
+            </button>
+            <small className='form-text text-danger'>{this.state.err}</small>
+          </form>
+        </div>
       </div>
     );
   }
